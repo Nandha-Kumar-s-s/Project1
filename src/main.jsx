ccddad7ec1486941ae0988cee84f2d13d004278d
NandhaKumar <116733545+Nandha-Kumar-s-s@users.noreply.github.com>
2024-07-25 15:35:06 +0530

    Revert "merging from nadhakumar to master 22/07/2024"

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 // import './index.css'
-import PivotTable from "./PivotTable";
-import ManageColumn from "./ManageColumn";
-import { ReportProvider } from "./ReportContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ReportProvider>
-    <div>
-      <ManageColumn />
-      <PivotTable />
-    </div>
-  </ReportProvider>
+    <App />
   </React.StrictMode>,
 )
